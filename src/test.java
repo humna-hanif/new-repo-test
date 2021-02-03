fec331e4b3540f6cf0a47202fa5cc7d25d737993
Humna Hanif <humnahanif@Humnas-MacBook-Pro.local>
2021-02-02 19:58:48 -0500

    added color and age question with unique outputs

--- src/test.java
+++ src/test.java
@@ -5,9 +5,26 @@ public class test {
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Hello. This is a change to my blank file.");
 		System.out.println("Let's see if it works!");
+		
 		System.out.println("What is your name?");
 		String name = sc.nextLine();
 		System.out.println("Hello " + name + "!");
+		
+		System.out.println("What is your favorite color?");
+		String color = sc.nextLine();		
+		System.out.println(color + " is a great color.");
+		
+		System.out.println("How old are you?");
+		int age = sc.nextInt();
+		
+		if (age <= 12)
+		{
+			System.out.println("You are very young!");
+		} else if (age >=12 && age <=17)
+		{
+			System.out.println("Living the teen life huh!");
+		} else
+			System.out.println("Wow you're an Adult!");
 	}
 
 }
