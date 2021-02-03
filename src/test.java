3f308885c2605170e86811372daeda67da43dd03
Humna Hanif <humnahanif@Humnas-MacBook-Pro.local>
2021-02-02 19:40:26 -0500

    added two println text

--- src/test.java
+++ src/test.java
@@ -2,7 +2,8 @@
 public class test {
 
 	public static void main(String[] args) {
-
+		System.out.println("Hello. This is a change to my blank file.");
+		System.out.println("Let's see if it works!");
 	}
 
 }
