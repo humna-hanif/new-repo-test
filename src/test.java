c71e3ca7361c789a42448a5f90183e7488ea536c
Humna Hanif <humnahanif@Humnas-MacBook-Pro.local>
2021-02-02 19:44:39 -0500

    added a println text

--- src/test.java
+++ src/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello. This is a change to my blank file.");
 		System.out.println("Let's see if it works!");
+		System.out.println("Forgot to take a screenshot of the terminal, here we go again!");
 	}
 
 }
